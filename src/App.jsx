4c265fa9d484ea24bbbef88f959a5ebcfebcb060
wei <5b1g0032@stust.edu.tw>
2025-06-11 17:40:18 +0800

    更新 App 元件標題為「5b1g0032 莊小煒」，並在 Vite 配置中添加空行

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>123456789陳小明</h1>
+      <h1>5b1g0032 莊小煒</h1>
       <h2>我的第一個元件：Slider</h2>
       <div>
         <p>
